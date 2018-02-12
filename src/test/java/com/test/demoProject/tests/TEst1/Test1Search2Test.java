13b0d17a1c81df95c408a2e6fb1eceeec0794624
Strugariu, Florin <Florin.Strugariu@garmin.com>
2018-02-12 10:42:59 -0600

    add more tests

---
+++ src/test/java/com/test/demoProject/tests/TEst1/Test1Search2Test.java
@@ -0,0 +1,88 @@
+package com.test.demoProject.tests.TEst1;
+
+import com.test.demoProject.steps.HomeStep;
+import com.test.demoProject.steps.SearchResultsSteps;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(SerenityRunner.class)
+public class Test1Search2Test {
+
+	@Managed()
+	public WebDriver webdriver;
+
+	@Steps
+	public HomeStep homeStep;
+
+	@Steps
+	public SearchResultsSteps searchResultsSteps;
+
+	@Test
+	public void Search2Test_searchThis() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+
+	@Test
+	public void Search2Test_searchThis1() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search2Test_searchThis2() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search2Test_searchThis3() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search2Test_searchThis4() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search2Test_searchThis5() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search2Test_searchThis6() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search2Test_searchThis7() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search2Test_searchThis8() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+}
---
+++ src/test/java/com/test/demoProject/tests/test7/test7Search4Test.java
@@ -0,0 +1,88 @@
+package com.test.demoProject.tests.test4;
+
+import com.test.demoProject.steps.HomeStep;
+import com.test.demoProject.steps.SearchResultsSteps;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(SerenityRunner.class)
+public class test7Search4Test {
+
+	@Managed()
+	public WebDriver webdriver;
+
+	@Steps
+	public HomeStep homeStep;
+
+	@Steps
+	public SearchResultsSteps searchResultsSteps;
+
+	@Test
+	public void Search4Test_searchThis() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+
+	@Test
+	public void Search4Test_searchThis1() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search4Test_searchThis2() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search4Test_searchThis3() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search4Test_searchThis4() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search4Test_searchThis5() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search4Test_searchThis6() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search4Test_searchThis7() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+	@Test
+	public void Search4Test_searchThis8() {
+		homeStep.openFirstPage();
+		homeStep.typeSearch("GPS");
+
+		searchResultsSteps.checkSearchResults();
+	}
+}
